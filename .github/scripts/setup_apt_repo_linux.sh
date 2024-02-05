23393890e6573c4487899b0fc22335a3fe01188e
Mark Hildebrand <mhildebrand@ucdavis.edu>
2024-02-05 09:51:29 -0800

    Add `sudo` to MKL install.

--- .github/scripts/setup_apt_repo_linux.sh
+++ .github/scripts/setup_apt_repo_linux.sh
@@ -7,6 +7,6 @@
 # Taken from: https://github.com/oneapi-src/oneapi-ci
 
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "deb https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
-apt-get update -o Dir::Etc::sourcelist="sources.list.d/oneAPI.list" -o APT::Get::List-Cleanup="0"
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/oneAPI.list" -o APT::Get::List-Cleanup="0"
