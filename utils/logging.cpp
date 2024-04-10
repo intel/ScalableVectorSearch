967689ab140fec943429cc86245bdb9c1b25581f
Mark Hildebrand <mark.hildebrand@intel.com>
2024-04-10 10:41:38 -0700

    Run LicenseBuilder. (#341)
    GitOrigin-RevId: a881be7be370f0207fe57914072d5175e0f7811c

--- utils/logging.cpp
+++ utils/logging.cpp
@@ -1,3 +1,14 @@
+/**
+ *    Copyright (C) 2023-present, Intel Corporation
+ *
+ *    You can redistribute and/or modify this software under the terms of the
+ *    GNU Affero General Public License version 3.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    version 3 along with this software. If not, see
+ *    <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+ */
+
 // A utility function used in an integration test for logging initialization.
 
 // svs
