71c0bc1e3c336d116f6efe60cda44c529559e815
Alexander Andreev <alexander.andreev@intel.com>
2024-05-07 09:22:46 -0700

    Update recipe deps

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -10,12 +10,18 @@
 # <https://www.gnu.org/licenses/agpl-3.0.en.html>.
 #
 
-export ONEAPI_INSTALL_DIR="$PWD/oneapi_install_dir"
+# install and source oneMKL
+export ONEAPI_INSTALL_DIR="$PREFIX/oneapi"
 wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/2f3a5785-1c41-4f65-a2f9-ddf9e0db3ea0/l_onemkl_p_2024.1.0.695_offline.sh
 bash l_onemkl_p_2024.1.0.695_offline.sh -a -s --eula accept --install-dir $ONEAPI_INSTALL_DIR
 rm l_onemkl_p_2024.1.0.695_offline.sh
 
 source $ONEAPI_INSTALL_DIR/setvars.sh
 
-export CMAKE_ARGS="$CMAKE_ARGS -DPYSVS_MICROARCHS=x86_64_v3;cascadelake;icelake;sapphirerapids"
+# pysvs building
+export CMAKE_ARGS="$CMAKE_ARGS -DPYSVS_MICROARCHS=cascadelake;icelake;sapphirerapids"
 $PYTHON -m pip install bindings/python
+
+# clean up oneAPI installation or 
+# conda-build will try to `patchelf` its libs with failure
+rm -rf $ONEAPI_INSTALL_DIR
