fdf936f1719f19667f3c5a740d55eb0e84461f2d
ethanglaser <42726565+ethanglaser@users.noreply.github.com>
2025-02-27 14:16:05 -0800

    update version to 0.0.7 (#90)
    Co-authored-by: ibhati <ishwar.s.bhati@intel.com>

--- bindings/python/tests/test_common.py
+++ bindings/python/tests/test_common.py
@@ -42,7 +42,7 @@ class CommonTester(unittest.TestCase):
     #####
 
     def test_version(self):
-        self.assertEqual(svs.library_version(), "v0.0.6")
+        self.assertEqual(svs.library_version(), "v0.0.7")
 
     def test_conversion(self):
         # signed
