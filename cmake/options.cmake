56a84e7de2ff62ee5704e0847da9932ff1bcc76c
Alexander Andreev <alexander.andreev@intel.com>
2025-04-24 00:32:36 -0700

    TEMP: enable cmake verbosity options

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -19,8 +19,12 @@ set(svs_options_cmake_included true)
 
 # Default to Release build
 if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE Release)
+    set(CMAKE_BUILD_TYPE Debug)
 endif()
+
+# Enable all possible debugging options
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g -O0")
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g -O0")
 
 #####
 ##### Official Options
