db50e4e9afba36276bb8f47871cf7aef798c88f6
Aaron Lin <aaron.dl.lin@intel.com>
2025-08-14 16:18:19 -0700

    move omp flag to optionally defined

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -73,11 +73,6 @@ option(SVS_INITIALIZE_LOGGER
     ON # enabled by default
 )
 
-option(SVS_ENABLE_OMP
-    "Support OpenMP threadpool."
-    OFF # disable by default
-)
-
 #####
 ##### Experimental
 #####
@@ -143,12 +138,6 @@ if (SVS_INITIALIZE_LOGGER)
     target_compile_options(${SVS_LIB} INTERFACE -DSVS_INITIALIZE_LOGGER=1)
 else()
     target_compile_options(${SVS_LIB} INTERFACE -DSVS_INITIALIZE_LOGGER=0)
-endif()
-
-if (SVS_ENABLE_OMP)
-    target_compile_options(${SVS_LIB} INTERFACE -DSVS_ENABLE_OMP=1)
-else()
-    target_compile_options(${SVS_LIB} INTERFACE -DSVS_ENABLE_OMP=0)
 endif()
 
 #####
