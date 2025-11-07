1d100af392a31218eb3eefc2968cd746c5cf81e3
Andreas Huber <9201869+ahuber21@users.noreply.github.com>
2025-11-07 10:14:08 +0100

    Replace macros with runtime_error_wrapper() for error handling (#214)
    I think this produces more readable code. And since
    `runtime_error_wrapper()` is an internal function, there shouldn't be
    any disadvantages for the shared lib.

--- bindings/cpp/src/dynamic_vamana_index.cpp
+++ bindings/cpp/src/dynamic_vamana_index.cpp
@@ -55,28 +55,25 @@ struct DynamicVamanaIndexManagerBase : public DynamicVamanaIndex {
     ~DynamicVamanaIndexManagerBase() override = default;
 
     Status add(size_t n, const size_t* labels, const float* x) noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        svs::data::ConstSimpleDataView<float> data{x, n, impl_->dimensions()};
-        std::span<const size_t> lbls(labels, n);
-        impl_->add(data, lbls);
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] {
+            svs::data::ConstSimpleDataView<float> data{x, n, impl_->dimensions()};
+            std::span<const size_t> lbls(labels, n);
+            impl_->add(data, lbls);
+        });
     }
 
     Status
     remove_selected(size_t* num_removed, const IDFilter& selector) noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        *num_removed = impl_->remove_selected(selector);
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] {
+            *num_removed = impl_->remove_selected(selector);
+        });
     }
 
     Status remove(size_t n, const size_t* labels) noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        std::span<const size_t> lbls(labels, n);
-        impl_->remove(lbls);
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] {
+            std::span<const size_t> lbls(labels, n);
+            impl_->remove(lbls);
+        });
     }
 
     Status search(
@@ -88,12 +85,11 @@ struct DynamicVamanaIndexManagerBase : public DynamicVamanaIndex {
         const SearchParams* params = nullptr,
         IDFilter* filter = nullptr
     ) const noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        // TODO wrap arguments into proper data structures in DynamicVamanaIndexImpl and
-        // here
-        impl_->search(n, x, k, distances, labels, params, filter);
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] {
+            // TODO wrap arguments into proper data structures in DynamicVamanaIndexImpl and
+            // here
+            impl_->search(n, x, k, distances, labels, params, filter);
+        });
     }
 
     Status range_search(
@@ -104,26 +100,19 @@ struct DynamicVamanaIndexManagerBase : public DynamicVamanaIndex {
         const SearchParams* params = nullptr,
         IDFilter* filter = nullptr
     ) const noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        // TODO wrap arguments into proper data structures in DynamicVamanaIndexImpl and
-        // here
-        impl_->range_search(n, x, radius, results, params, filter);
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] {
+            // TODO wrap arguments into proper data structures in DynamicVamanaIndexImpl and
+            // here
+            impl_->range_search(n, x, radius, results, params, filter);
+        });
     }
 
     Status reset() noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        impl_->reset();
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] { impl_->reset(); });
     }
 
     Status save(std::ostream& out) const noexcept override {
-        SVS_RUNTIME_TRY_BEGIN
-        impl_->save(out);
-        return Status_Ok;
-        SVS_RUNTIME_TRY_END
+        return runtime_error_wrapper([&] { impl_->save(out); });
     }
 };
 
@@ -146,20 +135,16 @@ Status DynamicVamanaIndex::build(
     const DynamicVamanaIndex::SearchParams& default_search_params
 ) noexcept {
     *index = nullptr;
-    SVS_RUNTIME_TRY_BEGIN
-    auto impl = std::make_unique<DynamicVamanaIndexImpl>(
-        dim, metric, storage_kind, params, default_search_params
-    );
-    *index = new DynamicVamanaIndexManager{std::move(impl)};
-    return Status_Ok;
-    SVS_RUNTIME_TRY_END
+    return runtime_error_wrapper([&] {
+        auto impl = std::make_unique<DynamicVamanaIndexImpl>(
+            dim, metric, storage_kind, params, default_search_params
+        );
+        *index = new DynamicVamanaIndexManager{std::move(impl)};
+    });
 }
 
 Status DynamicVamanaIndex::destroy(DynamicVamanaIndex* index) noexcept {
-    SVS_RUNTIME_TRY_BEGIN
-    delete index;
-    return Status_Ok;
-    SVS_RUNTIME_TRY_END
+    return runtime_error_wrapper([&] { delete index; });
 }
 
 Status DynamicVamanaIndex::load(
@@ -169,12 +154,11 @@ Status DynamicVamanaIndex::load(
     StorageKind storage_kind
 ) noexcept {
     *index = nullptr;
-    SVS_RUNTIME_TRY_BEGIN
-    std::unique_ptr<DynamicVamanaIndexImpl> impl{
-        DynamicVamanaIndexImpl::load(in, metric, storage_kind)};
-    *index = new DynamicVamanaIndexManager{std::move(impl)};
-    return Status_Ok;
-    SVS_RUNTIME_TRY_END
+    return runtime_error_wrapper([&] {
+        std::unique_ptr<DynamicVamanaIndexImpl> impl{
+            DynamicVamanaIndexImpl::load(in, metric, storage_kind)};
+        *index = new DynamicVamanaIndexManager{std::move(impl)};
+    });
 }
 
 // Specialization to build LeanVec-based Vamana index with specified leanvec dims
@@ -188,13 +172,12 @@ Status DynamicVamanaIndexLeanVec::build(
     const DynamicVamanaIndex::SearchParams& default_search_params
 ) noexcept {
     *index = nullptr;
-    SVS_RUNTIME_TRY_BEGIN
-    auto impl = std::make_unique<DynamicVamanaIndexLeanVecImpl>(
-        dim, metric, storage_kind, leanvec_dims, params, default_search_params
-    );
-    *index = new DynamicVamanaIndexLeanVecImplManager{std::move(impl)};
-    return Status_Ok;
-    SVS_RUNTIME_TRY_END
+    return runtime_error_wrapper([&] {
+        auto impl = std::make_unique<DynamicVamanaIndexLeanVecImpl>(
+            dim, metric, storage_kind, leanvec_dims, params, default_search_params
+        );
+        *index = new DynamicVamanaIndexLeanVecImplManager{std::move(impl)};
+    });
 }
 
 // Specialization to build LeanVec-based Vamana index with provided training data
@@ -208,15 +191,14 @@ Status DynamicVamanaIndexLeanVec::build(
     const DynamicVamanaIndex::SearchParams& default_search_params
 ) noexcept {
     *index = nullptr;
-    SVS_RUNTIME_TRY_BEGIN
-    auto training_data_impl =
-        static_cast<const LeanVecTrainingDataManager*>(training_data)->impl_;
-    auto impl = std::make_unique<DynamicVamanaIndexLeanVecImpl>(
-        dim, metric, storage_kind, training_data_impl, params, default_search_params
-    );
-    *index = new DynamicVamanaIndexLeanVecImplManager{std::move(impl)};
-    return Status_Ok;
-    SVS_RUNTIME_TRY_END
+    return runtime_error_wrapper([&] {
+        auto training_data_impl =
+            static_cast<const LeanVecTrainingDataManager*>(training_data)->impl_;
+        auto impl = std::make_unique<DynamicVamanaIndexLeanVecImpl>(
+            dim, metric, storage_kind, training_data_impl, params, default_search_params
+        );
+        *index = new DynamicVamanaIndexLeanVecImplManager{std::move(impl)};
+    });
 }
 
 } // namespace runtime
