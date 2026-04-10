71aee33fe68ad7612cc7e356d52273253218b2d1
ethanglaser <42726565+ethanglaser@users.noreply.github.com>
2026-04-10 16:01:10 -0700

    Apply suggestion from @ethanglaser

--- .github/scripts/test-cpp-runtime-bindings.sh
+++ .github/scripts/test-cpp-runtime-bindings.sh
@@ -35,7 +35,8 @@ conda install -y mkl=2025.3 mkl-devel=2025.3
 conda install -y /runtime_conda/libsvs-runtime-*.conda
 
 # Validate python and C++ tests against FAISS CI
-git clone https://github.com/facebookresearch/faiss.git
+TODO: clone main branch upon merge of https://github.com/facebookresearch/faiss/pull/4801
+git clone --branch ib/svs_ivf https://github.com/ibhati/faiss.git
 cd faiss
 
 echo "==============================================="
